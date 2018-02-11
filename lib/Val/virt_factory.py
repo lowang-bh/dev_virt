75b3ad7b664298269970bcd54fdda62fa9bfa778
mac@lowang <lhui_wang@163.com>
2018-02-11 18:41:31 +0800

    firt commit for init

---
+++ lib/Val/virt_factory.py
@@ -0,0 +1,10 @@
+#########################################################################
+# File Name: virt_factory.py
+# Attentions: this file is used to generate relative production using diffent driver
+# Author: longhui
+# Created Time: 2018-02-08 11:29:36
+#########################################################################
+#!/usr/bin/env python
+#! -*- coding: utf-8 -*-
+
+
