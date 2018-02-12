85d1fc638049163e5e4c1e19bff94fbe032878f5
lowang-bh <lhui_wang@163.com>
2018-02-12 15:20:14 +0800

    fix the file header

--- lib/Db/db_driver.py
+++ lib/Db/db_driver.py
@@ -1,10 +1,10 @@
+#!/usr/bin/env python
+#! -*- coding: utf-8 -*-
 #########################################################################
 # File Name: db_driver.py
 # Attantions: This file is used as driver to connect to DB
 # Author: longhui
 # Created Time: 2018-02-08 11:18:37
 #########################################################################
-#!/usr/bin/env python
-#! -*- coding: utf-8 -*-
 
 
