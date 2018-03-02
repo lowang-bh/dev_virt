7703564e0fc6072a1064228ffbfc6bd8b3fc97b9
lowang-bh <lhui_wang@163.com>
2018-03-02 20:22:32 +0800

    fix a bug in poweroff vm

--- lib/Val/Xen/virt_driver_xen_test.py
+++ lib/Val/Xen/virt_driver_xen_test.py
@@ -5,9 +5,55 @@
  Author: longhui
  Created Time: 2018-03-01 13:46:58
 '''
+import unittest
+import time
 from lib.Val.Xen.virt_driver_xen import XenVirtDriver
-virt_driver = XenVirtDriver()
-assert virt_driver.is_instance_exists("new_vm") == True
-assert virt_driver.delete_instance("new_vm") == True
-assert virt_driver.is_instance_exists("new_vm") == False
+
+
+class XenVirtDriverTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.virt_driver = XenVirtDriver(hostname="10.143.248.13", user="root", passwd="Mojiti!906")
+        self.vms = ['t2-dev16', 't2-jenkins-lain2', 't2-dev24', 't2-dev23', 't2-dev25', 't2-dev26', 't2-dev22']
+
+    def tearDown(self):
+        self.virt_driver.delete_handler()
+
+    def test_get_handler(self):
+        self.handler = self.virt_driver.get_handler()
+        self.assertIsNotNone(self.handler, "handler is None")
+
+    def test_get_vm_list(self):
+        dst_vms = self.virt_driver.get_vm_list()
+        for vm in self.vms:
+            self.assertEqual(vm in dst_vms, True, "%s not there" % vm)
+
+    def test_get_template_list(self):
+        templates = ["CentOS 7.2 for Lain"]
+        dst_templates = self.virt_driver.get_templates_list()
+        self.assertNotEqual(dst_templates, [], "get_templates_list() return []")
+        for templ in templates:
+            self.assertEqual(templ in dst_templates, True, "%s not there" % templ)
+
+    def test_is_instance_running(self):
+        for vm in self.vms:
+            self.assertEqual(self.virt_driver.is_instance_running(vm), True, "%s is not running" % vm)
+            self.assertEqual(self.virt_driver.get_vm_record(vm)['power_state'], "Running", "%s is not running" % vm)
+
+#    def test_power_on_vm(self):
+#        vm = "new_vm"
+#        self.assertEqual(self.virt_driver.is_instance_running(vm), False, "%s is already running" % vm)
+#        self.assertTrue(self.virt_driver.power_on_vm(vm), "%s power on failed" % vm)
+#        time.sleep(0.5)
+#        self.assertTrue(self.virt_driver.is_instance_running(vm), "%s should be in running" % vm)
+
+    def test_power_off_vm(self):
+        vm = "new_vm"
+        self.assertTrue(self.virt_driver.power_off_vm(vm), "%s power off failed" % vm)
+        time.sleep(0.5)
+        self.assertFalse(self.virt_driver.is_instance_running(vm), "%s should not be in running" % vm)
+
+
+if __name__ == "__main__":
+    unittest.main()
 
