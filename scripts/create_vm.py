9944daf3d3d07b4b8a0177052cce4de663f76346
lowang-bh <lhui_wang@163.com>
2018-03-05 13:11:40 +0800

    adjust the api in create_vm.py

--- scripts/create_vm.py
+++ scripts/create_vm.py
@@ -27,39 +27,60 @@ if __name__ == "__main__":
     parser.add_option("--ip", dest="ip",
                       help="IP for new VM.")
 
+    parser.add_option("--list-vm", dest="list_vm", action="store_true",
+                      help="List all the vms in server.")
+    parser.add_option("--list-templ", dest="list_templ", action="store_true",
+                      help="List all the templates in the server.")
+
     (options, args) = parser.parse_args()
     log.debug("options:%s, args:%s", str(options), str(args))
-    if options.vm_name is None:
-        parser.print_help()
-        exit(1)
 
     if options.host is not None and (options.user is None or options.passwd is None):
         log.fail("Please specify a user-name and passward for the given host:%s", options.host)
         exit(1)
 
-    if options.vm_name is not None and options.template is None:
-        log.fail("A template must be suppulied to create a new VM.")
-        exit(1)
-
-    new_vm_name, template_name = options.vm_name, options.template
     host_name = options.host
     user = options.user if options.user else "root"
     passwd = str(options.passwd).replace('\\', '') if options.passwd else ""
 
-    virt_driver = VirtFactory.get_virt_driver(host_name, user, passwd)
+    if options.list_vm:
+        virt_driver = VirtFactory.get_virt_driver(host_name, user, passwd)
+        all_vms = virt_driver.get_vm_list()
+        if all_vms:
+            log.info(str(all_vms))
+        else:
+            log.info("No VMs.")
 
-    if virt_driver.is_instance_exists(new_vm_name):
-        log.fail("There is already one VM named [%s]", new_vm_name)
-        exit(1)
-    if template_name not in virt_driver.get_templates_list():
-        log.fail("No template named: %s", template_name)
-        exit(1)
+    elif options.list_templ:
+        virt_driver = VirtFactory.get_virt_driver(host_name, user, passwd)
+        all_templs = virt_driver.get_templates_list()
+        if all_templs:
+            log.info(str(all_templs))
+        else:
+            log.info("No templates.")
 
-    ret = virt_driver.create_instance(new_vm_name, template_name)
-    if ret:
-        log.success("Create VM [%s] successfully.", new_vm_name)
-        exit(0)
+    elif options.vm_name is not None:
+        if options.template is None:
+            log.fail("A template must be suppulied to create a new VM.")
+            exit(1)
+        new_vm_name, template_name = options.vm_name, options.template
+        virt_driver = VirtFactory.get_virt_driver(host_name, user, passwd)
+
+        if virt_driver.is_instance_exists(new_vm_name):
+            log.fail("There is already one VM named [%s]", new_vm_name)
+            exit(1)
+        if template_name not in virt_driver.get_templates_list():
+            log.fail("No template named: %s", template_name)
+            exit(1)
+
+        ret = virt_driver.create_instance(new_vm_name, template_name)
+        if ret:
+            log.success("Create VM [%s] successfully.", new_vm_name)
+            exit(0)
+        else:
+            log.fail("Failed to create VM [%s].", new_vm_name)
+            exit(1)
     else:
-        log.fail("Failed to create VM [%s].", new_vm_name)
+        parser.print_help()
         exit(1)
 
