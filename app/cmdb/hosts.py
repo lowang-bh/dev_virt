2b3c4ee88b84762dabf213d294aefe5ce828c84b
mac@lowang <lhui_wang@163.com>
2018-03-22 19:32:34 +0800

    tested update api

--- app/cmdb/hosts.py
+++ app/cmdb/hosts.py
@@ -15,7 +15,7 @@ class Host(DatabaseDriver):
         super(Host, self).__init__(*args, **kwagrs)
         self.url = HOSTs_URL
 
-    def create(self, data):
+    def create(self, **kwargs):
         """
         write data to db
         :param url:
@@ -24,8 +24,9 @@ class Host(DatabaseDriver):
         """
         url = self.url
         db_name = self.db_name(url)
+        data = kwargs
 
-        log.debug("Create url: %s", url)
+        log.debug("Create url: %s, with data:%s", url, data)
         self.resp = self.session.post(url, data=data)
         log.info("Http return code: %s", self.resp.status_code)
         if self.resp.status_code == requests.codes.created or self.resp.status_code == requests.codes.ok:
@@ -51,21 +52,49 @@ class Host(DatabaseDriver):
         if self.resp.status_code == requests.codes.no_content:
             log.info("Delete data from database [%s] successfully.", db_name)
         elif self.resp.status_code == requests.codes.not_found:
-            log.warn("Not found when delete from [%s]: 404", db_name)
+            log.warn("Not found the record when delete from [%s]: 404", db_name)
         elif self.is_respond_error:
             log.warn("Delete data from database: [%s] failed, return code: %s.", db_name, self.resp.status_code)
             return False
 
         return True
 
-    def update(self, id=None, data=None):
+    def update(self, id=None, sn=None, hostname=None, data=None):
         """
-        update the data to a record with its primary key is pk
+        update the data to a record with its primary key is id, id/sn/hostname should not be changed
         :param id:
         :param data:
         :return: True or False
         """
-        raise NotImplementedError()
+        if id is None and sn is None and hostname is None:
+            log.error("Update to DB need an ID, a hostname or a SN to identify which record will be updated.")
+            return False
+        if not isinstance(data, dict):
+            log.error("Data should be a dict.")
+            return  False
+
+        query_list = self.query(id=id, sn=sn, hostname=hostname)
+        if not query_list:
+            log.error("No record found with ID:%s, hostname:%s, sn:%s", id, hostname, sn)
+            return False
+
+        modified = query_list[0]['modified']
+        record_id = query_list[0]['id']
+        data['modified'] = str(modified)
+
+        url = self.url + str(record_id) + "/"  # update url should be endwith "/"
+        log.debug("Patch url:%s, data:%s", url, data)
+        self.resp = self.session.patch(url, data=data)
+        if self.resp.status_code == requests.codes.ok:
+            log.info("Update successfully.")
+            return True
+        elif self.is_respond_error:
+            log.error("Update failed. Return code: %s", self.resp.status_code)
+            log.debug("Error content: %s", self.resp.content)
+            return False
+
+        return True
+
 
     def query(self, id=None, sn=None, hostname=None):
         """
@@ -172,8 +201,12 @@ if __name__ == "__main__":
 
     with VirtualHost() as test:
         queryDic = test.query(sn=testdata['sn'], hostname=testdata['hostname'])
-        print test.respond_data_count
-        deleteId = queryDic[0]['id']
+        if not test.respond_data_count:
+            test.create(**testdata)
+            queryDic = test.query(sn=testdata['sn'], hostname=testdata['hostname'])
+        deleteId = test.respond_data_list[0]['id']
+        test.update(id=deleteId, data={"hostname":"hostname", "first_ip":"10.101.10.10"})
+        virhost.delete(id=deleteId)
         virhost.delete(id=deleteId)
         test.create(**testdata)
         test.query(sn=testdata['sn'], hostname=testdata['hostname'])
