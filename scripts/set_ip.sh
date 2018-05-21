6b08643b08b4a22eb31529b46c7b39d002e88497
lowang-bh <lhui_wang@163.com>
2018-05-21 14:41:20 +0800

    add kvm host device list

--- scripts/set_ip.sh
+++ scripts/set_ip.sh
@@ -63,6 +63,8 @@ if [[ $mymac == 00:66* ]];then
     elif [[ $grep_res == IPADDR=* ]];then
         echo "origin IP on $myeth:$grep_res, set to new: $MyIP"
         sed -i "s/^IPADDR=.*$/IPADDR=$MyIP/" /etc/sysconfig/network-scripts/ifcfg-$myeth
+        sed -i "s/^GATEWAY=.*$/GATEWAY=$MyIP/" /etc/sysconfig/network-scripts/ifcfg-$myeth
+        sed -i "s/^BROADCAST=.*$/BROADCAST=$MyIP/" /etc/sysconfig/network-scripts/ifcfg-$myeth
     else
         echo "Already configed $myeth"
     fi
